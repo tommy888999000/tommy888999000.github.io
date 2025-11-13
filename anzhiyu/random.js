dedc91297a5a84bd53d1e545e2e0e29ad08e4beb
tommy888999000 <tommy888999000@gmail.com>
2025-11-13 16:12:16 +0800

    Site updated: 2025-11-13 16:12:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/13/这是一篇新的博文/","2025/11/13/hello-world/"];function toRandomPost(){
+var posts=["2025/11/11/我家的猫/","2025/11/07/一张好图/","2025/11/04/这是一篇新的博文/","2025/11/01/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
